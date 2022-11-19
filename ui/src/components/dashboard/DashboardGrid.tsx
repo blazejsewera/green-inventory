1c48e19e2fdbe16cdfedf3e74ea22481b60b91ce
Patryk Kokowicz <gryzonin@gmail.com>
2022-11-19 21:18:40 +0100

    feat: add DashboardGrid

---
+++ ui/src/components/dashboard/DashboardGrid.tsx
@@ -0,0 +1,24 @@
+import { Container, Grid } from '@mantine/core'
+import { StatsCard, StatsRingProps } from './StatsRing'
+
+export function DashboardGrid() {
+  const statsData: StatsRingProps = {
+    label: 'Demanded items',
+    demanded: 50,
+    alertLvl: 51,
+    color: 'red',
+  }
+
+  return (
+    <Container my="xl">
+      <Grid columns={2}>
+        <Grid.Col>
+          <StatsCard {...statsData} />
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <StatsCard {...statsData} />
+        </Grid.Col>
+      </Grid>
+    </Container>
+  )
+}
